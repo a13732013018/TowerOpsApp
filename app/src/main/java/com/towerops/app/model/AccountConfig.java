edf6171adf1f73636e0fb588256e2183919df87e
a13732013018 <a13732013018@gmail.com>
2026-03-16 15:20:22 +0800

    Update AccountConfig.java

--- app/src/main/java/com/towerops/app/model/AccountConfig.java
+++ app/src/main/java/com/towerops/app/model/AccountConfig.java
@@ -13,6 +13,7 @@ public class AccountConfig {
             {"wx-jinlz",         "z0J1CVrRPjfQgO4jhLuJwg%3D%3D"},
             {"wx-wangjj96",      "uwhpKacQXX1aC3eyE9rkQg%3D%3D"},
             {"%20%20wx-liusl35", "0b1XBanT0rLwBl%2BMIo87Lw%3D%3D"},
+            {"wx-wangsw9",       "z0J1CVrRPjfQgO4jhLuJwg%3D%3D"},
     };
 
     /** 返回下拉框显示名（脱敏展示，去除前缀空格） */
